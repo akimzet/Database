e3eddaa9ee36f79b0a90f0c55d23200f08b9c397
akimzet <akim18@horizon.csueastbay.edu>
2017-11-19 19:10:49 -0800

    Test

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+//LOAD the various controllers
+var ControllerDatabase = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+
+//MAY HAVE OTHER CODE in index.js
+
+
+//CODE to route /getAllRoutes to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
+//      and Render information iwith an ejs view
+router.get('/storeData', ControllerDatabase.storeData);
+
+module.exports = router;
