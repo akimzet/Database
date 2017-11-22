ca1924822e6c41cb1c5a75776dbdb5d67fe4d276
akimzet <akim18@horizon.csueastbay.edu>
2017-11-22 14:54:18 -0800

    Testing Customers, Billing, Shipping

--- controllers/database.js
+++ controllers/database.js
@@ -38,64 +38,64 @@ module.exports.storeData =  function (request, response) {
         };
 
 
-    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
-
-        var CUSTOMER_ID = Math.floor((Math.random() * 1000000000000) + 1);
-        var CUSTOMERS = db.collection('CUSTOMERS');
-        CUSTOMERS.insertOne(customerData, function (err, result)
-        {
-            if (err) throw err;
-        });
-
-
-
-
-        /*CUSTOMERS.deleteMany({}, function (err, result) {
-        if (err) throw err;
-        });*/
-
-
-
-
-
-        //get collection of routes
-        // var Routes = db.collection('Routes');
-
-
-        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        // var c = Routes.find({});
-
-        c.forEach(
-            function(myDoc) {
-                console.log( "name: " + myDoc.name );  //just  loging the output to the console
-            }
-        );
-
-
-        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
-        //   array called docs that you  forward to the  storeData.ejs view for use there
-        Routes.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('storeData', {results: docs});
-
-        });
-
-
-        //Showing in comments here some alternative read (find) requests
-        //this gets Routes where frequency>=10 and sorts by name
-        // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Routes by name
-        //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
-
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
+    // mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+    //     if(err) throw err;
+    //
+    //     var CUSTOMER_ID = Math.floor((Math.random() * 1000000000000) + 1);
+    //     var CUSTOMERS = db.collection('CUSTOMERS');
+    //     CUSTOMERS.insertOne(customerData, function (err, result)
+    //     {
+    //         if (err) throw err;
+    //     });
+    //
+    //
+    //
+    //
+    //     /*CUSTOMERS.deleteMany({}, function (err, result) {
+    //     if (err) throw err;
+    //     });*/
+    //
+    //
+    //
+    //
+    //
+    //     //get collection of routes
+    //     // var Routes = db.collection('Routes');
+    //
+    //
+    //     //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
+    //     //   this request and loop  is to display content in the  console log
+    //     // var c = Routes.find({});
+    //
+    //     c.forEach(
+    //         function(myDoc) {
+    //             console.log( "name: " + myDoc.name );  //just  loging the output to the console
+    //         }
+    //     );
+    //
+    //
+    //     //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+    //     //   array called docs that you  forward to the  storeData.ejs view for use there
+    //     Routes.find().toArray(function (err, docs) {
+    //         if(err) throw err;
+    //
+    //         response.render('storeData', {results: docs});
+    //
+    //     });
+    //
+    //
+    //     //Showing in comments here some alternative read (find) requests
+    //     //this gets Routes where frequency>=10 and sorts by name
+    //     // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
+    //     // this sorts all the Routes by name
+    //     //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
+    //
+    //
+    //     //close connection when your app is terminating.
+    //     db.close(function (err) {
+    //         if(err) throw err;
+    //     });
+    // });//end of connect
     mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
         if(err) throw err;
 
@@ -154,62 +154,62 @@ module.exports.storeData =  function (request, response) {
             if(err) throw err;
         });
     });//end of connect
-    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
-
-        var SHIPPING_ID = Math.floor((Math.random() * 1000000000000) + 1);
-        var SHIPPING = db.collection('SHIPPING');
-        SHIPPING.insertOne(shippingData, function (err, result)
-        {
-            if (err) throw err;
-        });
-
-
-
-
-        /*CUSTOMERS.deleteMany({}, function (err, result) {
-        if (err) throw err;
-        });*/
-
-
-
-
-
-        //get collection of routes
-        // var Routes = db.collection('Routes');
-
-
-        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        // var c = Routes.find({});
-
-        c.forEach(
-            function(myDoc) {
-                console.log( "name: " + myDoc.name );  //just  loging the output to the console
-            }
-        );
-
-
-        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
-        //   array called docs that you  forward to the  storeData.ejs view for use there
-        Routes.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('storeData', {results: docs});
-
-        });
-
-
-        //Showing in comments here some alternative read (find) requests
-        //this gets Routes where frequency>=10 and sorts by name
-        // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Routes by name
-        //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
-
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
+    // mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+    //     if(err) throw err;
+    //
+    //     var SHIPPING_ID = Math.floor((Math.random() * 1000000000000) + 1);
+    //     var SHIPPING = db.collection('SHIPPING');
+    //     SHIPPING.insertOne(shippingData, function (err, result)
+    //     {
+    //         if (err) throw err;
+    //     });
+    //
+    //
+    //
+    //
+    //     /*CUSTOMERS.deleteMany({}, function (err, result) {
+    //     if (err) throw err;
+    //     });*/
+    //
+    //
+    //
+    //
+    //
+    //     //get collection of routes
+    //     // var Routes = db.collection('Routes');
+    //
+    //
+    //     //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
+    //     //   this request and loop  is to display content in the  console log
+    //     // var c = Routes.find({});
+    //
+    //     c.forEach(
+    //         function(myDoc) {
+    //             console.log( "name: " + myDoc.name );  //just  loging the output to the console
+    //         }
+    //     );
+    //
+    //
+    //     //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+    //     //   array called docs that you  forward to the  storeData.ejs view for use there
+    //     Routes.find().toArray(function (err, docs) {
+    //         if(err) throw err;
+    //
+    //         response.render('storeData', {results: docs});
+    //
+    //     });
+    //
+    //
+    //     //Showing in comments here some alternative read (find) requests
+    //     //this gets Routes where frequency>=10 and sorts by name
+    //     // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
+    //     // this sorts all the Routes by name
+    //     //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
+    //
+    //
+    //     //close connection when your app is terminating.
+    //     db.close(function (err) {
+    //         if(err) throw err;
+    //     });
+    // });//end of connect
 };//end function
