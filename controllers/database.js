ed4de494f72429c015cfdcc1f6877796d55ec232
akimzet <akim18@horizon.csueastbay.edu>
2017-11-22 14:37:47 -0800

    Testing Customers, Billing, Shipping

--- controllers/database.js
+++ controllers/database.js
@@ -10,39 +10,6 @@ var mongoDBURI = process.env.MONGODB_URI || 'mongodb://jk4629:password@ds113626.
  *
  */
 module.exports.storeData =  function (request, response) {
-    var customerData =
-        {
-            _id: CUSTOMER_ID,
-            FIRSTNAME: request.body.FIRSTNAME,
-            LASTNAME: request.body.LASTNAME,
-            STREET: request.body.SHIPPING_STREET1 + ' ' + request.body.SHIPPING_STREET2,
-            CITY: request.body.SHIPPING_CITY,
-            STATE: request.body.SHIPPING_STATE,
-            ZIP: request.body.SHIPPING_ZIP,
-            PHONE: request.body.PHONE
-        };
-
-
-    var billingData =
-        {
-            _id: BILLING_ID,
-            CUSTOMER_ID: CUSTOMER_ID,
-            CREDITCARDTYPE: request.body.CREDITCARDTYPE,
-            CREDITCARDNUM: request.body.CREDITCARDNUM,
-            CREDITCARDEXP: request.body.CREDITCARDEXP,
-            CREDITCARDSECURITYNUM: request.body.CREDITCARDSECURITYNUM
-        };
-
-    var shippingData =
-        {
-            _id: SHIPPING_ID,
-            CUSTOMER_ID: CUSTOMER_ID,
-            SHIPPING_STREET: request.body.SHIPPING_STREET1 + ' ' + request.body.SHIPPING_STREET2,
-            SHIPPING_CITY: request.body.SHIPPING_CITY,
-            SHIPPING_STATE: request.body.SHIPPING_STATE,
-            SHIPPING_ZIP: request.body.SHIPPING_ZIP
-        };
-
     mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
         if(err) throw err;
 
@@ -55,24 +22,55 @@ module.exports.storeData =  function (request, response) {
         var SHIPPING = db.collection('SHIPPING');
         var ORDERS = db.collection('ORDERS');
 
+        var customerData =
+            {
+                _id: CUSTOMER_ID,
+                FIRSTNAME: request.body.FIRSTNAME,
+                LASTNAME: request.body.LASTNAME,
+                STREET: request.body.SHIPPING_STREET1 + ' ' + request.body.SHIPPING_STREET2,
+                CITY: request.body.SHIPPING_CITY,
+                STATE: request.body.SHIPPING_STATE,
+                ZIP: request.body.SHIPPING_ZIP,
+                PHONE: request.body.PHONE
+            };
 
+
+        var billingData =
+            {
+                _id: BILLING_ID,
+                CUSTOMER_ID: CUSTOMER_ID,
+                CREDITCARDTYPE: request.body.CREDITCARDTYPE,
+                CREDITCARDNUM: request.body.CREDITCARDNUM,
+                CREDITCARDEXP: request.body.CREDITCARDEXP,
+                CREDITCARDSECURITYNUM: request.body.CREDITCARDSECURITYNUM
+            };
+
+        var shippingData =
+            {
+                _id: SHIPPING_ID,
+                CUSTOMER_ID: CUSTOMER_ID,
+                SHIPPING_STREET: request.body.SHIPPING_STREET1 + ' ' + request.body.SHIPPING_STREET2,
+                SHIPPING_CITY: request.body.SHIPPING_CITY,
+                SHIPPING_STATE: request.body.SHIPPING_STATE,
+                SHIPPING_ZIP: request.body.SHIPPING_ZIP
+            };
 
 
 
         CUSTOMERS.insertOne(customerData, function (err, result)
         {
             if (err) throw err;
-        })
+        });
 
         BILLING.insertOne(billingData, function (err, result)
         {
             if (err) throw err;
-        })
+        });
 
         SHIPPING.insertOne(shippingData, function (err, result)
         {
             if (err) throw err;
-        })
+        });
 
 
 
